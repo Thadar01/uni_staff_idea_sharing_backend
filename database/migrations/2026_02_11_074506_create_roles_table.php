8cccab0674fb704b8a8ad921cc8dd28b0b50e0a7
laravel-first-tuto-admin <eainteaintkyawlin@gmail.com>
2026-02-11 14:31:19 +0630

    roles table create

---
+++ database/migrations/2026_02_11_074506_create_roles_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+   
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id('roleID');        // Primary Key
+            $table->string('roleName');  // Role name
+            $table->timestamps();  
+        });
+    }
+
+ 
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
