1757f04787588fc10016f6642846ca2286678737
Thadar01 <thadarshinhtet2019@gmail.com>
2026-04-11 13:54:00 +0630

    new idea change

--- resources/views/emails/new_idea.blade.php
+++ resources/views/emails/new_idea.blade.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>New Idea Submission</title>
 </head>
+
 <body>
     <h2>A new idea has been submitted!</h2>
     <p><strong>Title:</strong> {{ $idea->title }}</p>
-    <p><strong>Submitted by:</strong> {{ $idea->isAnonymous ? 'Anonymous' : ($idea->staff ? $idea->staff->staffName : 'Unknown User') }}</p>
+    <p><strong>Submitted by:</strong>
+        @if($idea->staff)
+            {{ $idea->staff->staffName }}{{ $idea->isAnonymous ? ' (Anonymous)' : '' }}
+        @else
+            Unknown User
+        @endif
+    </p>
 
     <br>
     <p><strong>Description:</strong></p>
@@ -15,6 +23,7 @@
     </div>
 
     <br>
-    <p>Please log in to the portal to review the idea, assign categories if needed, or monitor activity.</p>
+    <p>Please log in to the portal to review the idea, assign closure-settings if needed, or monitor activity.</p>
 </body>
+
 </html>
